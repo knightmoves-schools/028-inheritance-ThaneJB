8131d9de526412c2ed0703394536ade9496a3a9a
Thane <thabay0606@students.maquoketaschools.org>
2026-03-20 12:56:58 -0500

    Made Ageable class and gave it an Age method.

--- Entities.cs
+++ Entities.cs
@@ -1,11 +1,19 @@
 ﻿namespace knightmoves;
-public class Company{
+
+public class Ageable{
+    protected int Year;
+
+    protected Age(int year){
+        Year = year;
+    }
+}
+
+public class Company : Ageable{
     private string Name;
     private string Id;
-    private int Year;
     private decimal TaxRate;
 
-    public Company(string name, string taxId, int yearEstablished, decimal taxRate){
+    public Company(string name, string taxId, int yearEstablished, decimal taxRate) : base(year){
         Name = name;
         Id = taxId;
         Year = yearEstablished;
@@ -13,13 +21,12 @@ public class Company{
     }
 }
 
-public class Person{
+public class Person : Ageable{
     private string Name;
     private string Id;
-    private int Year;
     private decimal TaxRate;
 
-    public Person(string name, string ssn, int birthYear, decimal taxRate){
+    public Person(string name, string ssn, int birthYear, decimal taxRate) : base(year){
         Name = name;
         Id = ssn;
         Year = birthYear;
@@ -27,12 +34,12 @@ public class Person{
     }
 }
 
-public class Car{
+public class Car : Ageable{
     private string Name;
     private string Id;
     private int Year;
 
-    public Car(string model, string vin, int year){
+    public Car(string model, string vin, int year) : base(year){
         Name = model;
         Id = vin;
         Year = year;
